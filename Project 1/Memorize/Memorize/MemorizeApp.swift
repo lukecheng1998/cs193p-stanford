a44dcaac6f95669b0975ae6b8ade00b543771cc4
Luke Cheng <lukecheng1998@gmail.com>
2021-06-01 11:13:19 -0400

    init commit

---
+++ Project 1/Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Luke Cheng on 5/31/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
